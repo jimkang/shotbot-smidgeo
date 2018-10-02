a43345c3cd73112bb9f4806672574f4c9e5e0fb7
Jim Kang <jimkang@gmail.com>
2018-10-02 19:15:48 -0400

    Use probability table to pick kits.

--- behaviors/colorerx-behavior.js
+++ behaviors/colorerx-behavior.js
@@ -2,111 +2,231 @@ var probable = require('probable');
 var randomIA = require('random-internet-archive');
 var sb = require('standard-bail')();
 
-var kits = [
-  {
-    quantMin: 16,
-    quantMax: 192,
-    grayscale: 'yes',
-    showBaseChance: 50
-  },
-  {
-    query: 'animals',
-    quantMin: 16,
-    quantMax: 160,
-    showBaseChance: 50,
-    grayscale: 'yes'
-  },
-  {
-    query: 'nature',
-    quantMin: 16,
-    quantMax: 128,
-    showBaseChance: 50,
-    grayscale: 'yes'
-  },
-  {
-    query: 'sculpture',
-    quantMin: 16,
-    quantMax: 192,
-    showBaseChance: 50,
-    grayscale: 'yes'
-  },
-  {
-    query: 'landscape',
-    quantMin: 16,
-    quantMax: 192,
-    showBaseChance: 50,
-    grayscale: 'yes'
-  },
-  {
-    query: 'portrait',
-    quantMin: 16,
-    quantMax: 192,
-    showBaseChance: 80,
-    grayscale: 'yes'
-  },
-  {
-    collection: 'metropolitanmuseumofart-gallery',
-    quantMin: 32,
-    quantMax: 192,
-    showBaseChance: 50,
-    grayscale: 'yes'
-  },
-  {
-    collection: 'unsplash',
-    quantMin: 32,
-    quantMax: 192,
-    showBaseChance: 50,
-    grayscale: 'yes'
-  },
-  {
-    collection: 'nasanaturalhazards',
-    quantMin: 32,
-    quantMax: 192,
-    showBaseChance: 50,
-    grayscale: 'yes'
-  },
-  {
-    collection: 'visibleearthcollection',
-    quantMin: 32,
-    quantMax: 192,
-    showBaseChance: 50,
-    grayscale: 'yes'
-  },
-  {
-    collection: 'nasaimageofthedaygallery',
-    quantMin: 32,
-    quantMax: 192,
-    showBaseChance: 50,
-    grayscale: 'yes'
-  },
-  {
-    collection: 'flickrcommons',
-    quantMin: 32,
-    quantMax: 192,
-    showBaseChance: 50,
-    grayscale: 'yes'
-  },
-  {
-    collection: 'afghanmediaresourcecenter',
-    quantMin: 16,
-    quantMax: 160,
-    showBaseChance: 50,
-    grayscale: 'yes'
-  },
-];
+var kitTable = probable.createTableFromSizes([
+  [
+    10,
+    // Unspecified
+    {
+      quantMin: 16,
+      quantMax: 192,
+      grayscale: 'yes',
+      showBaseChance: 50,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99
+    }
+  ],
+  [
+    100,
+    // Animals
+    {
+      query: 'animals',
+      quantMin: 16,
+      quantMax: 160,
+      showBaseChance: 50,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+
+      grayscale: 'yes'
+    }
+  ],
+  [
+    150,
+    // Nature
+    {
+      query: 'nature',
+      quantMin: 16,
+      quantMax: 128,
+      showBaseChance: 30,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes',
+      minimumValueDifference: 0.4
+    }
+  ],
+  [
+    200,
+    // Sculpture
+    {
+      query: 'sculpture',
+      quantMin: 16,
+      quantMax: 192,
+      showBaseChance: 30,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes'
+    }
+  ],
+  [
+    100,
+    // Landscape
+    {
+      query: 'landscape',
+      quantMin: 16,
+      quantMax: 192,
+      showBaseChance: 50,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes',
+      minimumValueDifference: 0.4
+    }
+  ],
+  [
+    150,
+    // Portrait
+    {
+      query: 'portrait',
+      quantMin: 16,
+      quantMax: 192,
+      showBaseChance: 80,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes'
+    }
+  ],
+  [
+    100,
+    // Metropolitan Museum of Art
+    {
+      collection: 'metropolitanmuseumofart-gallery',
+      quantMin: 32,
+      quantMax: 192,
+      showBaseChance: 50,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes'
+    }
+  ],
+  [
+    200,
+    // Unsplash
+    {
+      collection: 'unsplash',
+      quantMin: 32,
+      quantMax: 192,
+      showBaseChance: 40,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes'
+    }
+  ],
+  [
+    70,
+    // Name
+    {
+      collection: 'nasanaturalhazards',
+      quantMin: 32,
+      quantMax: 128,
+      showBaseChance: 30,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes'
+    }
+  ],
+  [
+    200,
+    // Name
+    {
+      collection: 'visibleearthcollection',
+      quantMin: 32,
+      quantMax: 128,
+      showBaseChance: 40,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes',
+      minimumValueDifference: 0.4
+    }
+  ],
+  [
+    100,
+    // Name
+    {
+      collection: 'nasaimageofthedaygallery',
+      quantMin: 32,
+      quantMax: 192,
+      showBaseChance: 50,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes'
+    }
+  ],
+  [
+    100,
+    // Name
+    {
+      collection: 'flickrcommons',
+      quantMin: 32,
+      quantMax: 192,
+      showBaseChance: 50,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes'
+    }
+  ],
+  [
+    200,
+    // Name
+    {
+      collection: 'afghanmediaresourcecenter',
+      quantMin: 16,
+      quantMax: 160,
+      showBaseChance: 50,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes',
+      minimumValueDifference: 0.4
+    }
+  ],
+  [
+    100,
+    // Drawings
+    {
+      query: 'drawing',
+      quantMin: 8,
+      quantMax: 192,
+      showBaseChance: 25,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'no'
+    }
+  ],
+  [
+    100,
+    // Etchings
+    {
+      query: 'etching',
+      quantMin: 8,
+      quantMax: 192,
+      showBaseChance: 25,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'no'
+    }
+  ],
+  [
+    100,
+    // Cat
+    {
+      query: 'cat',
+      quantMin: 8,
+      quantMax: 192,
+      showBaseChance: 50,
+      overlayOpacityMin: 30,
+      overlayOpacityMax: 99,
+      grayscale: 'yes'
+    }
+  ]
+]);
 
 function generateImageURL(done) {
-  var kit = probable.pickFromArray(kits);
+  var kit = kitTable.roll();
   randomIA(
     {
-      // TODO: Set up specific probabilities for collections.
-      // collection: probable.pickFromArray(['mathematicsimage', 'brooklynmuseum', 'coverartarchive', 'flickrcommons', 'flickr-ows', 'nasa', 'solarsystemcollection']),
       collection: kit.collection,
       query: kit.query,
       mediatype: 'image',
       fileExtensions: ['jpg', 'jpeg', 'png'],
       minimumSize: 50000,
-      maximumSize: 3000000
+      maximumSize: 2500000
     },
     sb(assembleImageURL, done)
   );
@@ -129,7 +249,9 @@ function generateImageURL(done) {
     done(null, {
       url: colorerURL,
       altText: text,
-      caption: text + ` <a href="${detailsURL}">Source</a>, <a href="${colorerURL}">Colorer</a>`
+      caption:
+        text +
+        ` <a href="${detailsURL}">Source</a>, <a href="${colorerURL}">Colorer</a>`
     });
   }
 }
@@ -137,15 +259,52 @@ function generateImageURL(done) {
 function generateRun(kit) {
   return {
     renderer: 'replacer',
-    //quant: probable.roll(3) == 0 ? probable.rollDie(200) : probable.pickFromArray([16, 32, 64, 64, 64, 96, 96, 128, 128, 128]),
     quant: kit.quantMin + probable.roll(kit.quantMax - kit.quantMin),
     grayscale: kit.grayscale,
     recolorMode: 'random',
     showBase: probable.roll(100) < kit.showBaseChance ? 'yes' : 'no',
-    opacityPercentOverBase: probable.rollDie(70) + 30,
-    minimumValueDifference: 0.2
+    opacityPercentOverBase: kit.overlayOpacityMin + probable.roll(kit.overlayOpacityMax - kit.overlayOpacityMin),
+    minimumValueDifference: kit.minimumValueDifference || 0.2
   };
 }
+
+module.exports = {
+  //postingTargets: ['archive', 'mastodon', 'twitter'],
+  postingTargets: ['archive'],
+  generateImageURL,
+  shouldAutoCrop: true,
+  webimageOpts: {
+    viewportOpts: {
+      width: 1280
+    },
+    screenshotOpts: {
+      fullPage: true
+    },
+    autocrop: {
+      cropOnlyFrames: false
+    }
+  },
+  archive: {
+    name: 'Colorer Bot',
+    title: 'Colorer Bot (experimental)',
+    idPrefix: 'coloring-',
+    homeLink: 'https://smidgeo.com/bots/colorerx/',
+    rootPath: '/usr/share/nginx/html/bots/colorerx',
+    footerHTML: `<footer>
+    <p>By <a href="https://smidgeo.com/notes/deathmtn">Jim Kang</a> and Alex Dorsk</p>
+    <p><a href="http://jimkang.com/colorer-web/">Do your own recoloring!</a></p>
+    <p><a href="https://smidgeo.com/bots/colorerx/rss/index.rss">Subscribe to RSS.</a></p>
+</footer>`,
+    maxEntriesPerPage: 20,
+    generateRSS: true,
+    fileAbstractionType: 'LocalGit',
+    archiveBaseURL: 'https://smidgeo.com/bots/colorerx',
+    rssFeedOpts: {
+      feed_url: 'https://smidgeo.com/bots/colorerx/rss/index.rss',
+      site_url: 'https://smidgeo.com/bots/colorerx/'
+    }
+  }
+};
 
 module.exports = {
   //postingTargets: ['archive', 'mastodon', 'twitter'],
